e57c85c82f45cab805f9a7b6193a3634d47365b4
ismail <coderismail22@gmail.com>
2024-03-10 16:13:42 +0600

    debouncing-learning-complete

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+// Select Button Using DOM
 const button = document.getElementById("btn");
 
-button.addEventListener("click", function () {
-  console.log("Button Clicked");
-});
+// Debounce Definition
+function debounce(fn, delay) {
+  let timeoutId;
+  return function () {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      fn();
+    }, delay);
+  };
+}
+// User Click
+button.addEventListener(
+  "click",
+  debounce(() => {
+    console.log("Button Clicked");
+  }, 1000)
+);
